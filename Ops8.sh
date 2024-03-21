5d1afbbed3614d8671a01d01ea3c8dc69522bffe
molly <seelmolly95@gmail.com>
2024-03-21 18:34:25 -0500

    Ops8.sh

---
+++ Ops8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Lets create a script that would work like a DDOS attack by using a while loop
+#In this script we want to contiune to ping our personal pc in a infitine while loop
+#Somebody that had a control of a bot net could set up this script on thousands of computer and ping sites till they are overloaded and crash
+# To End the loop try pressing control z or control c
+# Stretch Goal:
+# Can you do this with an until loop to have it execute a specfic number of times?
+while [ 1 = 1 ] 
+do 
+ping facebook.com 
+done 
