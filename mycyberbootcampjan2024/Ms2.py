fc79a4578f15d7bcc1eaff3ccb7280277af9a5be
molly <seelmolly95@gmail.com>
2024-04-17 18:45:52 -0500

    updates

---
+++ mycyberbootcampjan2024/Ms2.py
@@ -0,0 +1,29 @@
+# Objectives
+# Create if statements using these logical conditionals below. Each statement should print information to the screen depending on if the condition is met.
+
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+# Create an if statement using a logical conditional of your choice and include elif keyword that executes when other conditions met or and else when no condition is met.
+a = int(input("Enter a number "))
+b = int(input("Enter a number "))
+if a == 7:
+    print(a)
+elif a != b:
+    print("they are the same") 
+else: 
+    print(b)    
+
+# Stretch Goals (Optional Objectives)
+# Pursue stretch goals if you are a more advanced user or have remaining lab time.
+
+# Create an if statement with two conditions by using and between conditions.
+
+# Create an if statement with two conditions by using or between conditions.
+
+# Create a nested if statement.
+
+# Hint:  a = int(input("Enter a number "))
