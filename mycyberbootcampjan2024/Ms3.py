c13e1c640c709bf82492db63b569d86eed94e957
molly <seelmolly95@gmail.com>
2024-04-22 18:56:19 -0500

    updates

---
+++ mycyberbootcampjan2024/Ms3.py
@@ -0,0 +1,30 @@
+# Here is a short story:
+# Once upon a time in Appleland, John had three apples, Mary had five apples, and Adam had six apples. They were all very happy and lived for a long time. End of story.
+# Your task is to:
+# create the variables: john, mary, and adam;
+john = 3
+mary = 5
+adam = 6
+# assign values to the variables. The values must be equal to the numbers of fruit possessed by John, Mary, and Adam respectively;
+# having stored the numbers in the variables, print the variables on one line, and separate each of them with a comma;
+print(john, mary, adam)
+# now create a new variable named total_apples equal to the addition of the three previous variables.
+total_apples = john + mary + adam
+# print the value stored in total_apples to the console;
+print("total number of apples:")
+
+# experiment with your code: create new variables, assign different values to them, and perform various arithmetic operations on them (e.g., +, -, *, /, //, etc.). Try to print a string and an integer together on one line, e.g., "Total number of apples:" and total_apples.
+a = 31
+b = 5
+c = 16
+sum=a-b
+diff=c-a
+product=b*a
+quotient=a*c
+remainder=a%b
+floordivision=c//b
+print("the difference obetween",a,"and",b, "and", c,"is:",sum)
+print("the product of",c,"and",b,"and", a,"is:",product)
+print("the quotient of", a,"and",c,"and", b,"is:",quotient)
+print("the remainder of",a,"and",b,"and", c,"is:",remainder)
+print("the floor division between",c,"and",b,"and", a, "is:",floordivision)
