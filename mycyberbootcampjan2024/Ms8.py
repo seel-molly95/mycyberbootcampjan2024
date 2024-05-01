28c0686e1bcdb0d92789b0c6a6d242b092f5619b
molly <seelmolly95@gmail.com>
2024-05-01 18:58:57 -0500

    updates

--- mycyberbootcampjan2024/Ms8.py
+++ mycyberbootcampjan2024/Ms8.py
@@ -2,9 +2,12 @@
 #  Having counted to five, 
 # the program should print to the screen the final message "Ready or not, here I come!"
 import time
+print(time.ctime())
 
 #Start code below this line:
 for count in range(1, 6):
     print(count, "mississippi")
     time.sleep(2)
 print("ready or not here i come")
+
+print(time.ctime())
